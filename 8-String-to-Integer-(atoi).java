0a86c046f2c8563beb37c106be8bb89dbbb74055
Deepakkumar S <135637489+deepakkumar11022005@users.noreply.github.com>
2024-08-20 15:07:35 +0530

    [8] [Time Beats: 5.12%] [Memory Beats: 79.62%] - LeetPush

---
+++ 8-String-to-Integer-(atoi).java
@@ -0,0 +1,36 @@
+class Solution {
+    public int myAtoi(String s) {
+      
+        int n = s.length();
+
+        int sum = 0;
+        s = s.trim();
+          if (s.length() == 0)
+            return 0;
+        boolean minus = false;
+        int i = 0;
+        if (s.charAt(0) == '-') {
+            minus = true;
+            i = 1;
+        }
+        else if (s.charAt(0) == '+') {
+           
+            i = 1;
+        }
+
+        for (; i < s.length(); i++) {
+
+            if (Character.isDigit(s.charAt(i))) {
+                int digit = s.charAt(i) - '0';
+                if (sum > (Integer.MAX_VALUE - digit) / 10) {
+                    return minus ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                }
+                sum = sum * 10 + (digit);
+            } else
+                break;
+        }
+        if (minus)
+            sum *= -1;
+        return sum;
+    }
+}
