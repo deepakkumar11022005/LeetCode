27cc347ba0622a8095632058cfd6c5a34fb65efb
Deepakkumar S <135637489+deepakkumar11022005@users.noreply.github.com>
2025-01-03 20:28:53 +0530

    Time: 3 ms (18.07%), Space: 41.7 MB (46.72%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -38,31 +38,33 @@ class Solution {
         }
         temp.end = true;
     }
-    public int getPre(int ind,String s)
-    {
-        Node temp=root;
-        int count=0;
-        int max=Integer.MIN_VALUE;
-        int n=s.length();
-        while(temp.map!=null && temp.map.size()==1 && ind<n)
-        {
-            char c=s.charAt(ind);
-            
-            if(!temp.map.containsKey(c) || max> temp.map.get(c).count) break;
-            max=Math.max(max, temp.map.get(c).count);
+
+    public int getPre(int ind, String s) {
+        Node temp = root;
+        int count = 0;
+        int max = Integer.MIN_VALUE;
+        int n = s.length();
+        while (temp.map != null && temp.map.size() == 1 && ind < n) {
+            char c = s.charAt(ind);
+
+            if (!temp.map.containsKey(c) || max > temp.map.get(c).count)
+                break;
+            max = Math.max(max, temp.map.get(c).count);
             // System.out.println(c);
             count++;
             ind++;
-            temp=temp.map.get(c);
+            temp = temp.map.get(c);
         }
         return count;
     }
-    
+
     public String longestCommonPrefix(String[] strs) {
-           for(String s:strs){
-           if(s.equals("")) return "";
-           insert(s);}
-        
-            return strs[0].substring(0,getPre(0,strs[0]));
+        for (String s : strs) {
+            if (s.equals(""))
+                return "";
+            insert(s);
+        }
+
+        return strs[0].substring(0, getPre(0, strs[0]));
     }
 }
