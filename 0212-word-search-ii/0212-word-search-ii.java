da1e5ccfc6a54e77de0fd0c87a63e9d1a6c28168
Deepakkumar S <135637489+deepakkumar11022005@users.noreply.github.com>
2025-01-05 17:49:33 +0530

    Time: 679 ms (23.85%), Space: 47.5 MB (32.93%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,63 +1,64 @@
-class Node{
-    boolean end=false;
-    Node arr[]=new Node[26];
+class Node {
+    boolean end = false;
+    Node[] arr = new Node[26];
 }
+
 class Solution {
     private Node root;
-    int row[]={1,-1,0,0};
-    int col[]={0,0,1,-1};
-     int n;
-        int m;
-    public Solution(){
-        root=new Node();
+    private int[] row = {1, -1, 0, 0};
+    private int[] col = {0, 0, 1, -1};
+    private int n;
+    private int m;
+
+    public Solution() {
+        root = new Node();
     }
-    public void insert(String s){
-        Node temp=root;
-        int n=s.length();
-        for(int i=0;i<n;i++)
-        {
-            int ind=s.charAt(i)-'a';
-            if(temp.arr[ind]==null){
-                temp.arr[ind]=new Node();
+
+    public void insert(String s) {
+        Node temp = root;
+        for (int i = 0; i < s.length(); i++) {
+            int ind = s.charAt(i) - 'a';
+            if (temp.arr[ind] == null) {
+                temp.arr[ind] = new Node();
             }
-            temp=temp.arr[ind];
+            temp = temp.arr[ind];
         }
-        temp.end=true;
+        temp.end = true;
     }
-    public void dfs(Node root,char[][] board,int a,int b,String temp,Set<String> res, boolean vis[][]){
-        if(root==null) return;
-        if(root.end)
-        {
-            res.add(temp);
-            // return;
+
+    public void dfs(Node root, char[][] board, int a, int b, StringBuilder temp, Set<String> res, boolean[][] vis) {
+        if (root == null) return;
+        if (root.end) {
+            res.add(temp.toString());
         }
-        vis[a][b]=true;
-        for(int i=0;i<4;i++)
-        {
-            int r=row[i]+a;
-            int c=col[i]+b;
-            if(r>=0 && r<n && c>=0 && c<m && !vis[r][c] && root.arr[board[r][c]-'a']!=null)
-            {
-                dfs(root.arr[board[r][c]-'a'],board,r,c,temp+board[r][c],res,vis);
+        vis[a][b] = true;
+        for (int i = 0; i < 4; i++) {
+            int r = row[i] + a;
+            int c = col[i] + b;
+            if (r >= 0 && r < n && c >= 0 && c < m && !vis[r][c] && root.arr[board[r][c] - 'a'] != null) {
+                temp.append(board[r][c]);
+                dfs(root.arr[board[r][c] - 'a'], board, r, c, temp, res, vis);
+                temp.deleteCharAt(temp.length() - 1); // Backtrack
             }
         }
-        vis[a][b]=false;
+        vis[a][b] = false;
     }
+
     public List<String> findWords(char[][] board, String[] words) {
-        for(String s:words)
-        insert(s);
-          n=board.length;
-          m=board[0].length;
-        Set<String> res=new HashSet<>();
-        boolean vis[][]=new boolean[n][m];
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                
-                if(root.arr[board[i][j]-'a']!=null)
-                {
-                    dfs(root.arr[board[i][j]-'a'],board,i,j,""+board[i][j],res,vis);
+        if (board == null || board.length == 0 || board[0].length == 0 || words == null || words.length == 0) {
+            return new ArrayList<>();
+        }
+        for (String s : words) {
+            insert(s);
+        }
+        n = board.length;
+        m = board[0].length;
+        Set<String> res = new HashSet<>();
+        boolean[][] vis = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (root.arr[board[i][j] - 'a'] != null) {
+                    dfs(root.arr[board[i][j] - 'a'], board, i, j, new StringBuilder().append(board[i][j]), res, vis);
                 }
             }
         }
