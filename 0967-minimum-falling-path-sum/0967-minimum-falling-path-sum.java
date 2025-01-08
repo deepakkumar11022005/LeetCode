cd7929d6fc011058f96e778474858e6cf94a6323
Deepakkumar S <135637489+deepakkumar11022005@users.noreply.github.com>
2025-01-08 21:46:11 +0530

    Time: 1 ms (99.86%), Space: 45.9 MB (5.79%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,17 +1,18 @@
 class Solution {
-    public int dfs(int nums[][], int num, int ind, int n, int sum,Integer dp[][]) {
+    public int dfs(int nums[][], int num, int ind, int n, Integer dp[][]) {
         if (ind < 0 || ind >= n)
             return Integer.MAX_VALUE;
         if (num == n - 1) {
-            return sum + nums[num][ind];
+            return nums[num][ind];
         }
         if (dp[num][ind] != null)
-            return dp[num][ind] + sum;
-        dp[num][ind]=Math.min(Math.min(
-                dfs(nums, num + 1, ind - 1, n, nums[num][ind] + sum,dp),
-                dfs(nums, num + 1, ind, n, nums[num][ind] + sum,dp)),
-                dfs(nums, num + 1, ind + 1, n, nums[num][ind] + sum,dp))-sum;
-        return  dp[num][ind]+sum;
+            return dp[num][ind] ;
+        int left= dfs(nums, num + 1, ind - 1, n,dp);
+        int st= dfs(nums, num + 1, ind, n,dp);
+        int right= dfs(nums, num + 1, ind + 1, n,dp);
+
+       dp[num][ind]=nums[num][ind]+Math.min(Math.min(left,st),right);
+        return  dp[num][ind];
     }
 
     public int minFallingPathSum(int[][] nums) {
@@ -19,7 +20,7 @@ class Solution {
         int min = Integer.MAX_VALUE;
         Integer dp[][]=new Integer[n][n];
         for (int i = 0; i < n; i++) {
-            int m = dfs(nums, 0, i, n, 0,dp);
+            int m = dfs(nums, 0, i, n,dp);
             min = Math.min(min, m);
         }
         return min;
