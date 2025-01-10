4d5ae27fc00c188418c849d1763dbfd8381150b2
Deepakkumar S <135637489+deepakkumar11022005@users.noreply.github.com>
2025-01-10 22:58:33 +0530

    Time: 16 ms (22.09%), Space: 53.9 MB (89.53%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.java
+++ 0952-word-subsets/0952-word-subsets.java
@@ -1,38 +1,31 @@
 class Solution {
-    public List<String> wordSubsets(String[] words1, String[] words2) {
-        int[] maxCharFreq = new int[26];
-        int[] tempCharFreq = new int[26];
-
-        for (String word : words2) {
-            Arrays.fill(tempCharFreq, 0);
-            for (char ch : word.toCharArray()) {
-                tempCharFreq[ch - 'a']++;
+    public List<String> wordSubsets(String[] arr1, String[] arr2) {
+        int arr[] = new int[26];
+        for (String s : arr2) {
+            int n = s.length();
+            int temp[] = new int[26];
+            for (int i = 0; i < n; i++) {
+                temp[s.charAt(i) - 'a']++;
             }
-            for (int i = 0; i < 26; ++i) {
-                maxCharFreq[i] = Math.max(maxCharFreq[i], tempCharFreq[i]);
-            }
-        }
-
-        List<String> universalWords = new ArrayList<>();
-
-        for (String word : words1) {
-            Arrays.fill(tempCharFreq, 0);
-            for (char ch : word.toCharArray()) {
-                tempCharFreq[ch - 'a']++;
-            }
-
-            boolean isUniversal = true;
-            for (int i = 0; i < 26; ++i) {
-                if (maxCharFreq[i] > tempCharFreq[i]) {
-                    isUniversal = false;
-                    break;
-                }
-            }
-            if (isUniversal) {
-                universalWords.add(word);
+            for (int i = 0; i < 26; i++) {
+                arr[i] = Math.max(arr[i], temp[i]);
             }
         }
-
-        return universalWords;
+        List<String> l = new ArrayList<>();
+        for (String s : arr1) {
+            int n = s.length();
+            int temp[] = new int[26];
+            for (int i = 0; i < n; i++) {
+                temp[s.charAt(i) - 'a']++;
+            }
+            boolean f = true;
+            for (int i = 0; i < 26; i++) {
+                if (arr[i] > temp[i])
+                    f = false;
+            }
+            if (f)
+                l.add(s);
+        }
+        return l;
     }
 }
