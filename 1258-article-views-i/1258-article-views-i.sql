2b98e55a9d36692f3db4bdb07bdc2d432d101732
Deepakkumar S <135637489+deepakkumar11022005@users.noreply.github.com>
2025-02-12 18:31:36 +0530

    Time: 1264 ms (7.45%), Space: 0B (100%) - LeetHub

---
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+select distinct author_id  id from Views where author_id in (SELECT viewer_id FROM Views where  author_id=viewer_id ) order by author_id ;
+
