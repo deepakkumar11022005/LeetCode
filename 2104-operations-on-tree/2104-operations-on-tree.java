ba13c2bb6f1e1d50d096b357c95e014aeb2f0f40
Deepakkumar S <135637489+deepakkumar11022005@users.noreply.github.com>
2025-02-21 11:44:48 +0530

    Time: 186 ms (39.69%), Space: 51.1 MB (41.75%) - LeetHub

--- 2104-operations-on-tree/2104-operations-on-tree.java
+++ 2104-operations-on-tree/2104-operations-on-tree.java
@@ -1,72 +1,82 @@
 class LockingTree {
     int par[];
     int lock[];
-    List<List<Integer>> adj=new ArrayList<>();
+    List<List<Integer>> adj = new ArrayList<>();
+
     public LockingTree(int[] parent) {
-        par=parent;
-        int n=parent.length;
-        lock=new int[n];
-        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
-        for(int i=1;i<n;i++) adj.get(parent[i]).add(i);
-        Arrays.fill(lock,-1);
+        par = parent;
+        int n = parent.length;
+        lock = new int[n];
+        for (int i = 0; i < n; i++) {
+            lock[i]=-1;
+            adj.add(new ArrayList<>());
+        }
+        for (int i = 1; i < n; i++)
+            adj.get(parent[i]).add(i);
+        // Arrays.fill(lock, -1);
     }
-    
+
     public boolean lock(int num, int user) {
-          if( lock[num]!=-1 ) return false;
-          lock[num]=user; 
-          return true;
+        if (lock[num] != -1)
+            return false;
+        lock[num] = user;
+        return true;
     }
-    
+
     public boolean unlock(int num, int user) {
-          if(lock[num]==-1 || lock[num]!=user  ) return false;
-          lock[num]=-1;
-          return true;
+        if (lock[num] == -1 || lock[num] != user)
+            return false;
+        lock[num] = -1;
+        return true;
     }
-    public boolean checkParent(int node)
-    {
-        if(node==-1) return false;
-        if(lock[node]!=-1) return true;
-        if(checkParent(par[node])) return true;
+
+    public boolean checkParent(int node) {
+        if (node == -1)
+            return false;
+        if (lock[node] != -1)
+            return true;
+        if (checkParent(par[node]))
+            return true;
         return false;
     }
-    public boolean checkChild(int num)
-    {
-        Queue<Integer> q=new LinkedList<>();
+
+    public boolean checkChild(int num) {
+        Queue<Integer> q = new LinkedList<>();
         q.add(num);
-        int countLockNode=0;
-        while(!q.isEmpty())
-        {
-            int temp=q.poll();
-            if(lock[temp]!=-1) countLockNode++;
-            for(int i:adj.get(temp))
-            {
+        int countLockNode = 0;
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+            if (lock[temp] != -1)
+                countLockNode++;
+            for (int i : adj.get(temp)) {
                 q.add(i);
             }
         }
-        return 1>countLockNode;
+        return 1 > countLockNode;
     }
-    public void makeChildUnlock(int num)
-    {
-        Queue<Integer> q=new LinkedList<>();
+
+    public void makeChildUnlock(int num) {
+        Queue<Integer> q = new LinkedList<>();
         q.add(num);
-       
-        while(!q.isEmpty())
-        {
-            int temp=q.poll();
-            lock[temp]=-1;
-            for(int i:adj.get(temp))
-            {
+
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+            lock[temp] = -1;
+            for (int i : adj.get(temp)) {
                 q.add(i);
             }
         }
         return;
     }
+
     public boolean upgrade(int num, int user) {
-          if(lock[num]!=-1) return false;
-          if(checkParent(num) || checkChild(num)) return false;
-          makeChildUnlock(num);
-          lock[num]=user;
-          return true;
+        if (lock[num] != -1)
+            return false;
+        if (checkParent(num) || checkChild(num))
+            return false;
+        makeChildUnlock(num);
+        lock[num] = user;
+        return true;
     }
 }
 
