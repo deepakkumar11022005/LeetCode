896d05db93dab76366da451bfab7957f6f2bccd7
Deepakkumar S <135637489+deepakkumar11022005@users.noreply.github.com>
2025-03-01 22:41:53 +0530

    Time: 1 ms (96.37%), Space: 44.3 MB (12.05%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -7,23 +7,14 @@ class Solution {
                 nums[i + 1] = 0;
             }
         }
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                stack.add(nums[i]);
-            }
+        int res[]=new int[n];
+        
+        int k=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>0)
+            res[k++]=nums[i];
         }
-        int size=stack.size();
-        int i = size-1;
-        while (!stack.isEmpty()) {
-            nums[i] = stack.pop();
-            i--;
-        }
-        i=size;
-        while (i < n) {
-            nums[i] = 0;
-            i++;
-        }
-        return nums;
+        return res;
     }
 }
