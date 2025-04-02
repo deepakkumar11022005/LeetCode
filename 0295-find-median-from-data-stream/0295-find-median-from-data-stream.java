dbede4ea46335d2fb613f2c2a92e3107c5d081d3
Deepakkumar S <135637489+deepakkumar11022005@users.noreply.github.com>
2025-04-02 15:32:46 +0530

    Time: 95 ms (69.55%), Space: 63.9 MB (43.14%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,44 +1,43 @@
 class MedianFinder {
     PriorityQueue<Integer> maxHeap;
     PriorityQueue<Integer> minHeap;
+
     public MedianFinder() {
-        maxHeap=new PriorityQueue<>((a,b)-> b-a);
-        minHeap=new PriorityQueue<>((a,b)-> a-b);
+        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        minHeap = new PriorityQueue<>((a, b) -> a - b);
     }
-    
+
+    public void swap(PriorityQueue<Integer> maxHeap, PriorityQueue<Integer> minHeap) {
+        if (!maxHeap.isEmpty() && !minHeap.isEmpty()) {
+            if (maxHeap.peek() > minHeap.peek()) {
+                minHeap.add(maxHeap.poll());
+                maxHeap.add(minHeap.poll());
+            }
+        }
+
+    }
+
     public void addNum(int num) {
         maxHeap.add(num);
-        if((maxHeap.size()-minHeap.size())>1) {
+        if ((maxHeap.size() - minHeap.size()) > 1) {
             minHeap.add(maxHeap.poll());
-            if(!maxHeap.isEmpty() && !minHeap.isEmpty()){
-                if(maxHeap.peek()>minHeap.peek()){
-                    minHeap.add(maxHeap.poll());
-                    maxHeap.add(minHeap.poll());
-                }
-            }
-        }
-        else{
-            if(!maxHeap.isEmpty() && !minHeap.isEmpty()){
-                if(maxHeap.peek()>minHeap.peek()){
-                    minHeap.add(maxHeap.poll());
-                    maxHeap.add(minHeap.poll());
-                }
-            }
-        }
+        }  swap(maxHeap,minHeap);
     }
-    
+
     public double findMedian() {
-        int size1=maxHeap.size();
-        int size2=minHeap.size();
+        int size1 = maxHeap.size();
+        int size2 = minHeap.size();
         // System.out.println(size1+" "+maxHeap +"  "+size2+" "+minHeap);
-        if((size1+size2)%2==0){
-            int a=0;
-            int b=0;
-            if(size1>0) a=maxHeap.peek();
-            if(size2>0) b=minHeap.peek();
-            return (double)(a+b)/2.0;
-        }else{
-            return size1>size2?(double)maxHeap.peek():(double)minHeap.peek();
+        if ((size1 + size2) % 2 == 0) {
+            int a = 0;
+            int b = 0;
+            if (size1 > 0)
+                a = maxHeap.peek();
+            if (size2 > 0)
+                b = minHeap.peek();
+            return (double) (a + b) / 2.0;
+        } else {
+            return size1 > size2 ? (double) maxHeap.peek() : (double) minHeap.peek();
         }
     }
 }
